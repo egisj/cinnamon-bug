7875bf77067aac404c9220f84098ae990b4c84b5
Egidijus Jankauskas <egidijus.jankauskas@deliveroo.co.uk>
2018-08-02 18:05:50 +0100

    Initial commit

---
+++ src/main/scala/CinnamonBug.scala
@@ -0,0 +1,38 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.{HttpApp, Route}
+import com.lightbend.cinnamon.akka.http.scaladsl.server.Endpoint
+
+import scala.concurrent.Future
+
+object CinnamonBug {
+  def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem("system")
+    implicit val executionContext = system.dispatcher
+
+    val app = new HttpApp {
+      override protected def routes: Route =
+        path("foo") {
+          Endpoint.withName("foo") {
+            get {
+              onSuccess(Future("s")) { _ =>
+                parameter('p) {
+                  case "a" =>
+                    Endpoint.withName("foo-a") {
+                      complete(StatusCodes.OK)
+                    }
+                  case "b" =>
+                    Endpoint.withName("foo-b") {
+                      complete(StatusCodes.OK)
+                    }
+                }
+              }
+            }
+          }
+        }
+    }
+
+    app.startServer("localhost", 8080, system)
+  }
+}
